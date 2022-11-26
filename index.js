4160270d146af827e8d93f3114ee26e8135c5d06
heyam <heyam976@gmail.com>
2022-11-26 19:46:45 +0200

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function receivesAFunction(callback){
+   callback();
+}
+function returnsANamedFunction(){
+  return function name () {
+    console.log("hi");
+  };
+
+}
+function returnsAnAnonymousFunction(){
+    return function  () {
+        console.log("heloo");
+      };
+}
